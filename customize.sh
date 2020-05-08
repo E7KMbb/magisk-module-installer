458864ecf64390912fd0c9779da33fe9ea335d1e
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-05-08 22:05:58 +0800

    Add files via upload
    Add some instructions

--- customize.sh
+++ customize.sh
@@ -1,4 +1,53 @@
 ##########################################################################################
+#
+# Magisk Module Installer Script
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+#
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure and implement callbacks in this file
+# 4. If you need boot scripts, add them into post-fs-data.sh or service.sh
+# 5. Add your additional or modified system properties into system.prop
+#
+##########################################################################################
+##########################################################################################
+#
+# The installation framework will export some variables and functions.
+# You should use these variables and functions for installation.
+#
+# ! DO NOT use any Magisk internal paths as those are NOT public API.
+# ! DO NOT use other functions in util_functions.sh as they are NOT public API.
+# ! Non public APIs are not guranteed to maintain compatibility between releases.
+#
+# Available variables:
+#
+# MAGISK_VER (string): the version string of current installed Magisk
+# MAGISK_VER_CODE (int): the version code of current installed Magisk
+# BOOTMODE (bool): true if the module is currently installing in Magisk Manager
+# MODPATH (path): the path where your module files should be installed
+# TMPDIR (path): a place where you can temporarily store files
+# ZIPFILE (path): your module's installation zip
+# ARCH (string): the architecture of the device. Value is either arm, arm64, x86, or x64
+# IS64BIT (bool): true if $ARCH is either arm64 or x64
+# API (int): the API level (Android version) of the device
+#
+# Availible functions:
+#
+# ui_print <msg>
+#     print <msg> to console
+#     Avoid using 'echo' as it will not display in custom recovery's console
+#
+# abort <msg>
+#     print error message <msg> to console and terminate installation
+#     Avoid using 'exit' as it will skip the termination cleanup steps
+#
+##########################################################################################
+
+##########################################################################################
 # SKIPUNZIP
 ##########################################################################################
 
@@ -46,8 +95,7 @@ REPLACE="
 # Permissions
 ##########################################################################################
 
-set_permissions() {
-  : # Remove this if adding to this function
+  # Remove this if adding to this function
 
   # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
   # Some examples:
@@ -66,5 +114,4 @@ set_permissions() {
   
   # The following is the default rule, DO NOT remove
   set_perm_recursive $MODPATH 0 0 0755 0644
-}
 
